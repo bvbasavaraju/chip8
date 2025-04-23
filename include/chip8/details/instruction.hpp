cb3292e608ab292426bbfab5dd285415950b9e98
batbug <bv.basavaraju@gmail.com>
2025-04-23 22:08:33 +0530

    :green_heart: CI build fix

--- include/chip8/details/instruction.hpp
+++ include/chip8/details/instruction.hpp
@@ -14,7 +14,7 @@ private:
     operands_((inst & 0x0FFF)) {}
 
 public:
-    static const auto decode(std::uint16_t inst) -> instruction_t {
+    static const auto decode(std::uint16_t inst) {
         return instruction_t{(inst)};
     }
 
