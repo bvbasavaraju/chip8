5215dc84ecb6f5f8904ed83feb221077cd7d91cf
batbug <bv.basavaraju@gmail.com>
2025-04-23 22:00:34 +0530

    :sparkles: :white_check_mark: added instruction features added test case fro instruction moved ops_t to new file so that it can be reused

--- include/chip8/details/instruction.hpp
+++ include/chip8/details/instruction.hpp
@@ -1,26 +1,108 @@
 #pragma once
 
-#include <opcodes.hpp>
+#include "opcodes.hpp"
 
 namespace chip8 {
 
 struct instruction_t {
 private:
-    std::uint8_t opcode_val;
-    operands_t operands;
+    std::uint8_t opcode;
+    std::uint16_t operands_;
 
-    instruction_t(std::uint8_t op_val)
-    : opcode_val(val) {}
+    instruction_t(std::uint16_t inst)
+    : opcode(((inst & 0xF000) >> 12)),
+    operands_((inst & 0x0FFF)) {}
 
 public:
-    static const auto decode(std::uin16_t inst) {
-        auto ret_val = instruction_t{(inst & 0xF000)};
-        opcode_t<opcode_val>::decode_operands(inst & 0x0FFF, ret_val.operands);
-        return inst;
+    static const auto decode(std::uint16_t inst) -> instruction_t {
+        return instruction_t{(inst)};
     }
 
+    template <typename Operations_t>
     const auto execute() const {
-        opcode_t<inst>{}(operands);
+        operands_t operands;
+
+
+        auto run  = [&]<std::uint8_t op_val>() -> void {
+            auto status = opcode_t<op_val>::decode_operands(operands_, operands);
+            if(!status) {
+                // TODO: Log status
+            }
+
+            opcode_t<op_val>::template validate_operands_and_execute<Operations_t>(operands);
+        };
+
+        switch (opcode)
+        {
+        case 0:
+            run.template operator()<0>();
+            break;
+
+        case 1:
+            run.template operator()<1>();
+            break;
+
+        case 2:
+            run.template operator()<2>();
+            break;
+
+        case 3:
+            run.template operator()<3>();
+            break;
+
+        case 4:
+            run.template operator()<4>();
+            break;
+
+        case 5:
+            run.template operator()<5>();
+            break;
+
+        case 6:
+            run.template operator()<6>();
+            break;
+
+        case 7:
+            run.template operator()<7>();
+            break;
+
+        case 8:
+            run.template operator()<8>();
+            break;
+
+        case 9:
+            run.template operator()<9>();
+            break;
+
+        case 0xA:
+            run.template operator()<0xA>();
+            break;
+
+        case 0xB:
+            run.template operator()<0xB>();
+            break;
+            
+
+        case 0xC:
+            run.template operator()<0xC>();
+            break;
+
+        case 0xD:
+            run.template operator()<0xD>();
+            break;
+
+        case 0xE:
+            run.template operator()<0xE>();
+            break;
+
+        case 0xF:
+            run.template operator()<0xF>();
+            break;
+        
+        default:
+            Operations_t::invalid_t::handle();
+            break;
+        }
     }
 }; 
 
---
+++ test/chip8/details/operations.hpp
@@ -0,0 +1,127 @@
+#pragma once
+
+struct ops_t {
+    struct call_t {
+        static bool flag;
+
+        static auto perform() -> void { flag = true; }
+    };
+
+    struct display_t {
+        static bool clear_screen_flag;
+        static bool draw_flag;
+
+        static auto clear_screen() -> void { clear_screen_flag = true; }
+        static auto draw(std::uint8_t pos_x, std::uint8_t pos_y, std::uint8_t height) -> void { draw_flag = true; }
+    };
+    
+    struct flow_t {
+        static bool jump_flag;
+        static bool jump_with_offset_flag;
+        static bool function_return_flag;
+        static bool function_call_at_flag;
+
+        static auto jump(std::uint16_t address) -> void { jump_flag = true; }
+        static auto jump_with_offset(std::uint16_t address) -> void { jump_with_offset_flag = true; }
+        static auto function_return() -> void { function_return_flag = true; }
+        static auto function_call_at(std::uint16_t address) -> void { function_call_at_flag = true; }
+    };
+
+    struct invalid_t {
+        static bool flag;
+
+        static auto handle() -> void { flag = true; }
+    };
+
+    struct conditional_t {
+        static bool skip_if_eq_to_val_flag;
+        static bool skip_if_not_eq_to_val_flag;
+        static bool skip_if_eq_to_reg_flag;
+        static bool skip_if_not_eq_to_reg_flag;
+
+        static auto skip_if_reg_equal_to_val(std::uint8_t x, std::uint8_t val) { skip_if_eq_to_val_flag = true; }
+        static auto skip_if_reg_not_equal_to_val(std::uint8_t x, std::uint8_t val) { skip_if_not_eq_to_val_flag = true; }
+        static auto skip_if_reg_equal_to_reg(std::uint8_t x, std::uint8_t y) { skip_if_eq_to_reg_flag = true; }
+        static auto skip_if_reg_not_equal_to_reg(std::uint8_t x, std::uint8_t y) { skip_if_not_eq_to_reg_flag = true; }
+    };
+
+    struct assign_t {
+        static bool set_reg_to_val_flag;
+        static bool set_reg_to_reg_flag;
+        static bool set_reg_to_rand_flag;
+
+        static auto set_reg_to_val(std::uint8_t x, std::uint8_t val) { set_reg_to_val_flag = true; }
+        static auto set_reg_to_reg(std::uint8_t x, std::uint8_t y) { set_reg_to_reg_flag = true; }
+        static auto set_reg_to_rand(std::uint8_t x, std::uint8_t mask) { set_reg_to_rand_flag = true; }
+    };
+
+    struct math_t {
+        static bool add_reg_with_val_with_carry_flag;
+        static bool add_reg_with_val_without_carry_flag;
+        static bool add_reg_with_reg_with_carry_flag;
+        static bool add_reg_with_reg_without_carry_flag;
+        static bool sub_reg_with_reg_with_carry_flag;
+        static bool sub_reg_with_reg_with_carry2_flag;
+
+        static auto add_reg_with_val_with_carry(std::uint8_t x, std::uint8_t val) { add_reg_with_val_with_carry_flag = true; }
+        static auto add_reg_with_val_without_carry(std::uint8_t x, std::uint8_t val) { add_reg_with_val_without_carry_flag = true; }
+        static auto add_reg_with_reg_with_carry(std::uint8_t x, std::uint8_t val) { add_reg_with_reg_with_carry_flag = true; }
+        static auto add_reg_with_reg_without_carry(std::uint8_t x, std::uint8_t val) { add_reg_with_reg_without_carry_flag = true; }
+        static auto sub_reg_with_reg_with_carry(std::uint8_t x, std::uint8_t val) { sub_reg_with_reg_with_carry_flag = true; }
+        static auto sub_reg_with_reg_with_carry2(std::uint8_t x, std::uint8_t val) { sub_reg_with_reg_with_carry2_flag = true; }
+    };
+
+    struct bitwise_t {
+        static bool or_flag;
+        static bool and_flag;
+        static bool xor_flag;
+        static bool right_shift_flag;
+        static bool left_shift_flag;
+
+        static auto or_op(std::uint8_t x, std::uint8_t y) { or_flag = true; }
+        static auto and_op(std::uint8_t x, std::uint8_t y) { and_flag = true; }
+        static auto xor_op(std::uint8_t x, std::uint8_t y) { xor_flag = true; }
+        static auto right_shift(std::uint8_t x, std::uint8_t y) { right_shift_flag = true; }
+        static auto left_shift(std::uint8_t x, std::uint8_t y) { left_shift_flag = true; }
+    };
+
+    struct keyop_t {
+        static bool skip_if_key_eq_to_reg_flag;
+        static bool skip_if_key_not_eq_to_reg_flag;
+        static bool read_key_stroke_flag;
+
+        static auto skip_if_key_eq_to_reg(std::uint8_t x) { skip_if_key_eq_to_reg_flag = true; }
+        static auto skip_if_key_not_eq_to_reg(std::uint8_t x) { skip_if_key_not_eq_to_reg_flag = true; }
+        static auto read_key_stroke(std::uint8_t x) { read_key_stroke_flag = true; }
+    };
+
+    struct mem_t {
+        static bool set_to_address_flag;
+        static bool add_reg_flag;
+        static bool set_to_sprite_char_flag;
+        static bool reg_dump_flag;
+        static bool reg_load_flag;
+
+        static auto set_to_address(std::uint16_t address) { set_to_address_flag = true; }
+        static auto add_reg(std::uint8_t x) { add_reg_flag = true; }
+        static auto set_to_sprite_char(std::uint8_t x) { set_to_sprite_char_flag = true; }
+        static auto reg_dump(std::uint8_t x) { reg_dump_flag = true; }
+        static auto reg_load(std::uint8_t x) { reg_load_flag = true; }
+    };
+
+    struct timer_t {
+        static bool get_delay_flag;
+        static bool set_delay_flag;
+        static bool set_sound_timer_flag;
+
+        static auto get_delay(std::uint8_t x) { get_delay_flag = true; }
+        static auto set_delay(std::uint8_t x) { set_delay_flag = true; }
+        static auto set_sound_timer(std::uint8_t x) { set_sound_timer_flag = true; }
+    };
+
+    struct bcd_t {
+        static bool flag;
+
+        static auto set(std::uint8_t x) { flag = true; }
+    };
+};
