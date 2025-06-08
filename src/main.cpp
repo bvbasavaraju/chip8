d141ce83892a7f4cf0bdb6cfb7da1299efa83a86
batbug <bv.basavaraju@gmail.com>
2025-06-08 19:55:09 +0530

    :sparkles: sdl3 added as submodule Simple example has been added

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <SDL3/SDL.h>
 
 int main() {
     std::cout << "Hello World\n";
+
+    if(!SDL_Init(SDL_INIT_VIDEO)) {
+        std::cout << "Failed to initialize the SDL library\n";
+        return -1;
+    }
+
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    if(!SDL_CreateWindowAndRenderer("Hello From SDL3", 640, 480, SDL_WINDOW_ALWAYS_ON_TOP, &window, &renderer)) {
+        std::cout << "Creating the Window or rendering failed\n";
+    }
+
+    SDL_Delay(3000);
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }
